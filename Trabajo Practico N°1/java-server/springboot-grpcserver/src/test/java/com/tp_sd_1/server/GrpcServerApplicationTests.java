6f522cf0a59ac24164219a52f3506ffe61ae6a2f
FrancoAguirre644 <francoaguirre644@gmail.com>
2021-08-31 11:49:56 -0300

    Hello World de grpc (Server) agregado

---
+++ Trabajo Practico N°1/java-server/springboot-grpcserver/src/test/java/com/tp_sd_1/server/GrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp_sd_1.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
